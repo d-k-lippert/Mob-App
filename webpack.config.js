e7c4e9712533eb71e5715cdc8481a4e937412275
d-k-lippert <51745385+d-k-lippert@users.noreply.github.com>
2019-06-15 15:02:54 +0200

    splitted index.jsx into components
    splitted index.jsx into components: create-elements.jsx for creating elements. this components provides the data for show-list.jsx. 
    the names of the declared constants have to be the same in both files. 
    delete-element.jsx deletes the added entry from the list. 
    Also added a main_style.css do to some styling - just added some regular flex-box classes to center the stuff up

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
                 use: [ 'style-loader', 'css-loader', 'sass-loader']
             },
             {
-                test: /\.css$/,
+                test: /.css$/,
                 use: [ 'style-loader', 'css-loader']
             },
         ],
