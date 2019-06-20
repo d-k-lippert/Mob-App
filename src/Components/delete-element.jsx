e3421dea5f11e4e57e9a0934713c43c3605e3137
d-k-lippert <51745385+d-k-lippert@users.noreply.github.com>
2019-06-20 13:29:03 +0200

    Add files via upload

--- src/Components/delete-element.jsx
+++ src/Components/delete-element.jsx
@@ -2,7 +2,7 @@ import {connect} from "react-redux";
 import React from "react";
 import {Button, Checkbox, Icon, Typography} from "antd";
 import '../main_style.css'
-
+import uuid from 'uuid/v4'
 
 const DELETE_ELEM = 'DELETE_ELEM'
 const EDIT_ELEM = 'EDIT_ELEM'
@@ -15,21 +15,30 @@ class Elem extends React.Component {
     constructor(props) {
         super(props)
 
-        this.artistRef = React.createRef()
-        this.dateRef = React.createRef()
-        this.genreRef = React.createRef()
-        this.locationRef = React.createRef()
+
+       /* this.artistRef = React.createRef()*/
+
+
+        this.state = {
+            artist: this.props.artist,
+            date: this.props.date,
+            genre: this.props.genre,
+            isInEditMode: false
+        }
     }
+
 
 
     deleteHandler = () => {
         this.props.dispatch({type: DELETE_ELEM, payload: this.props.id})
-    }
 
+
+    }
 
     /*handleTextChange(event) {
     this.props.setDataValue(event.target.value);
 }*/
+    /*
 
     onChange = () => {
         const artist = this.artistRef.current.value
@@ -39,33 +48,94 @@ class Elem extends React.Component {
 
         this.props.dispatch({type: EDIT_ELEM, payload: {artist, date, genre, location, id}})
     };
+*/
 
-
-/*
-    render() {
-        return (
-            <div>
-                <Paragraph editable={{ onChange: this.onChange }}>{this.state.str}</Paragraph>
-            </div>
-        );
+    /*
+        render() {
+            return (
+                <div>
+                    <Paragraph editable={{ onChange: this.onChange }}>{this.state.str}</Paragraph>
+                </div>
+            );
+        }
     }
-}
 
-    */
+        */
 
+
+
+
+    changeEditMode = () => {
+        this.setState({
+            isInEditMode: !this.state.isInEditMode
+        })
+    }
+
+    updateComponentValue = () => {
+        /*const artist = this.artistRef.current.value*/
+        this.setState({
+            isInEditMode: false,
+            artist: this.refs.artistInput.value,
+            date: this.refs.dateInput.value,
+            genre: this.refs.genreInput.value,
+
+
+        })
+
+            /*this.props.dispatch({type: EDIT_ELEM, payload: {artist,  id: uuid()}})*/
+    }
 
 
     render() {
 
-        return <div className="flex space-even v-center flex-direct">
+        //Render Edited
+        return this.state.isInEditMode ?
+
+            <div className="flex space-even v-center flex-direct">
+
+                <p> Artist:
+                <input type="text" defaultValue={this.state.artist} ref="artistInput"  /*ref={this.artistRef}*/ />
+                </p>
+                <input type="date" defaultValue={this.state.date} ref="dateInput"  /*ref={this.dateRef}*/ />
+
+
+
+                <label>Genre
+                    <select className="round-inputs" defaultValue={this.state.genre} ref="genreInput">
+                        <option value="Rock">Rock</option>
+                        <option value="Rap">Rap</option>
+                        <option value="Techno">Techno</option>
+                        <option value="Metal">Metal</option>
+                        <option value="Noise">Noise</option>
+                    </select>
+                </label>
+                
+                <p>Location: {this.props.location}</p>
+                <Button type="danger" onClick={this.changeEditMode}>Cancel</Button>
+                <Button type="primary" onClick={this.updateComponentValue.bind(this)}>Save Edit</Button>
+
+            </div>
+
+            :
+
+            //Render Default
+        <div className="flex space-even v-center flex-direct">
                 <Checkbox value={this.props.id}></Checkbox>
 
-                <Paragraph editable={{ onChange: this.onChange }} ref={this.artistRef} type="text">{this.props.artist}</Paragraph>
-                <p>Datum: {this.props.date}</p>
-                <p>Genre: {this.props.genre}</p>
+
+
+                <p>Artist: {this.state.artist}</p>
+
+                <p>Datum: {this.state.date}</p>
+
+
+                <p>Genre: {this.state.genre}</p>
+
+
                 <p>Location: {this.props.location}</p>
 
             <Button type="danger" onClick={this.deleteHandler}><Icon type="delete" /></Button>
+            <Button onClick={this.changeEditMode}><Icon type="edit" /></Button>
         </div>
 
     }
